6aa0c4e9da63789a80c9dc4c9495eb923ad768b7
szjkitti <szendi.joo.kitti@gmail.com>
2016-12-14 11:26:35 +0100

    Ötletláda
    A tartalmat ennek segítségével lehet módosítani: Név, email, tartalom

---
+++ admin_users_edit.php
@@ -0,0 +1,26 @@
+﻿<? include("vmi.php");?>
+<?
+  if(isset($_GET['id'])) {
+
+  $result = query("SELECT * FROM hirek WHERE `id` = ".$_GET['id']);
+
+}
+$akt_felhasznalo= $result->fetch_assoc();
+?>
+<div style="background-color:orange;">
+<h1> Tartalom módosítása </h1>
+
+<form action="felhasznalo_felvitel.php" method="POST">
+  Felhasználó neve:<br>
+  <input type="text" name="nev" value="<?= $akt_felhasznalo['nev']; ?>" >
+  <br>
+  Felhasználó emailje:<br>
+  <input type="email" name="email" value="<?= $akt_felhasznalo['email']; ?>">
+  <br>
+  Megjegyzés:<br>
+  <textarea name="tartalom" value="<?= $akt_felhasznalo['tartalom']; ?>" rows="15" cols="50">
+  </textarea><br>
+  <br><br>
+  <input type="submit" value="Felvisz">
+</form> 
+</div>
