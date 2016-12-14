320696b59c3db27406497ca6dba1535b3886455e
szjkitti <szendi.joo.kitti@gmail.com>
2016-12-14 11:25:03 +0100

    Ötletláda
    Tartalom hozzáadása: Név, email, tartalom

---
+++ admin_users_add.php
@@ -0,0 +1,17 @@
+﻿<div style="background-color:orange;">
+<h1> Megjegyzések </h1>
+
+<form action="felhasznalo_felvitel.php" method="POST">
+  Felhasználó neve:<br>
+  <input type="text" name="nev" value="" >
+  <br>
+  Felhasználó email címe:<br>
+  <input type="email" name="email" value="">
+  <br>
+  Megjegyzés:<br>
+  <textarea name="tartalom" value="" rows="15" cols="50">
+  </textarea><br>
+  <br><br>
+  <input type="submit" value="Felvisz">
+</form> 
+
